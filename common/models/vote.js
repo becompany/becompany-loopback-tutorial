09128b67f8fc4fd377c3bf6c0c38368599ff0092
Javier Puerto <javier.puerto@becompany.ch>
2016-04-21 16:21:30 +0200

    Add average rating.

--- common/models/vote.js
+++ common/models/vote.js
@@ -1,3 +1,24 @@
 module.exports = function(Vote) {
   Vote.validatesUniquenessOf('ip', {scopedTo: ['resource']});
+
+  Vote.voteAverage = function(resource, cb) {
+    Vote.find({where: {resource: resource}}, function (err, votes) {
+      var response = 0;
+      votes.forEach(function (vote) {
+        response += vote.value;
+      });
+      if (response) {
+        response = response / votes.length;
+      }
+      cb(null, response);
+    });
+  };
+  Vote.remoteMethod(
+    'voteAverage',
+    {
+      http: {path: '/voteAverage', verb: 'get'},
+      accepts: {arg: 'resource', type: 'string', http: {source :'query'}},
+      returns: {arg: 'voteAverage', type: 'numeric'}
+    }
+  );
 };
