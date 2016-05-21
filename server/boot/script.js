b3c847c567ec415994eebf7ff9590510e447af13
Javier Puerto <javier.puerto@becompany.ch>
2016-05-21 13:24:45 +0200

    Preload demo content.

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
   var User = app.models.User;
   var Role = app.models.Role;
+  var Survey = app.models.Survey;
+  var Resource = app.models.Resource;
   var RoleMapping = app.models.RoleMapping;
 
   User.create([
@@ -22,4 +24,30 @@ module.exports = function(app) {
       });
     });
   });
+
+  Survey.create([
+    {"topic": "fruit"},
+    {"topic": "color"}
+  ], function (err, surveys) {
+    if (err) throw err;
+
+    Resource.create([
+      {"name": "apple", "surveyId": surveys[0].id},
+      {"name": "watermelon", "surveyId": surveys[0].id},
+      {"name": "banana", "surveyId": surveys[0].id},
+      {"name": "kiwi", "surveyId": surveys[0].id}
+    ], function (err, fruits) {
+      if (err) throw err;
+    });
+
+    Resource.create([
+      {"name": "green", "surveyId": surveys[1].id},
+      {"name": "yellow", "surveyId": surveys[1].id},
+      {"name": "purple", "surveyId": surveys[1].id},
+      {"name": "red", "surveyId": surveys[1].id}
+    ], function (err, fruits) {
+      if (err) throw err;
+    })
+  })
+
 };
